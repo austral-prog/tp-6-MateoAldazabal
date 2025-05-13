b9696520426ed4d981013122e9bf38235dde7865
MateoAldazabal <maldazabal@mail.austral.edu.ar>
2025-05-13 18:09:29 +0000

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,31 +1,78 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    if len(list_to_remove_elements) < 1:
-		list_to_return = []
-    elif len(list_to_remove_elements) < 5:
-		list_to_return = del list_to_remove_elements[0]
-    elif len(list_to_remove_elements) < 6:
-		list_to_return = del list_to_remove_elements[0]
-		list_to_return = del list_to_remove_elements[5]
-    else:
-		list_to_return = del list_to_remove_elements[0]
-		list_to_return = del list_to_remove_elements[5]
-		list_to_return = del list_to_remove_elements[6]
-    return list_to_return
-
+	largo = len(list_to_remove_elements) 
+	if largo < 1:
+		list_to_remove_elements = []
+	elif largo < 5:
+		del list_to_remove_elements[0]
+	elif largo < 6:
+		del list_to_remove_elements[4]
+		del list_to_remove_elements[0]
+	else:
+		del list_to_remove_elements[5]
+		del list_to_remove_elements[4]
+		del list_to_remove_elements[0]
+	return list_to_remove_elements
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+	if len(list_to_add_elements) == 0:
+		list_to_add_elements = ['Pink', 'Yellow']
+	else:
+		list_to_add_elements.insert(0,"Pink")
+		list_to_add_elements.append("Yellow")
+	return list_to_add_elements
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
+	tamaño = len(list_to_check)
+	if tamaño == 0:
+		devolucion = 'true'
+	else:
+		devolucion = 'false'
+	return devolucion
+    
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+	l1 = len(list_to_compare1)
+	l2 = len(list_to_compare2)
+	if l1 < 3 and l2 < 3:
+		answer = "False"
+	elif l1 < 3 and l2 >=3:
+		answer = "False"
+	elif l1 >= 3 and l2 < 3:
+		answer = "False"
+	else:
+		answer = (list_to_compare1[2] == list_to_compare2[2])
+	return answer
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+	list1 = list_of_lists_to_modify[0]
+	list2 = list_of_lists_to_modify[1]
+	list3 = list_of_lists_to_modify[2]
+	new_list = []
+	len1 = len(list1)
+	len2 = len(list2)
+	len3 = len(list3)
+	if len1 == 0:
+		new_list1 = []
+	elif len1 == 1:
+		new_list1 = [list1[0]]
+	elif len1 > 1:
+		new_list1 = [list1[0], list1[1]]
+	
+	if len2 < 2:
+		new_list2 = []
+	elif len2 == 2:
+		new_list2 = [list2[1]]
+	elif len2 == 3:
+		new_list2 = [list2[1],list2[2]]
+	elif len2 > 3:
+		new_list2 = [list2[1],list2[2],list2[3]]
+
+	if len3 == 0:
+		new_list3 = []
+	elif len3 == 1:
+		new_list3 = [list3[-1]]
+	elif len3 > 1:
+		new_list3 = [list3[-2],list3[-1]]
+
+	return [new_list1, new_list2, new_list3]
